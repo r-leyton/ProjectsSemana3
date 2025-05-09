5fd800583bca8c0998c5f6025b5868c3c22cac30
Randall Leyton <LRosales-r@hotmail.com>
2025-05-08 21:02:32 -0500

    login-basico

--- app/src/main/java/com/example/projectssemana3/MainActivity.kt
+++ app/src/main/java/com/example/projectssemana3/MainActivity.kt
@@ -6,15 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val txtUsuario = findViewById<EditText>(R.id.etUsuario)
+        val txtContrasena = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val lblResultado = findViewById<TextView>(R.id.tvResultado)
+
+        btnLogin.setOnClickListener {
+            val usuario = txtUsuario.text.toString()
+            val contrasena = txtContrasena.text.toString()
+
+            if (usuario == "admin" && contrasena == "1234") {
+                lblResultado.text = "Inicio de sesión correcto"
+            } else {
+                lblResultado.text = "Usuario o contraseña incorrectos"
+            }
         }
     }
 }
